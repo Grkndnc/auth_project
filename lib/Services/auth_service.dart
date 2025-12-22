b259d9f98c640da5d29ac7a363550653d4f5abdc
Grkandnc <dincgurkan@gmail.com>
2025-12-22 15:05:51 +0300

    first commit

---
+++ lib/Services/auth_service.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+
+  // Sign in with email ans password
+  Future< User?> 
+}
